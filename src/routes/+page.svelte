2b103da2bffe89717c5391d68c41733a2d4ae8d3
ValerioMeschi <valerio.meschi@gmail.com>
2024-04-20 19:46:59 +0200

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+	let count = 1;
+	$: double = count * 2;
+
+	function increment() {
+		count++;
+	}
+	function doSomething() {
+		console.log(double);
+	}
+</script>
+
+{count} * 2 = {double}
+<button on:click={increment}>incrementme</button>
+<button on:click={doSomething}>clickme</button>
