2132321b58bb6a709de8248a4ff7a344a8f5e9aa
ValerioMeschi <valerio.meschi@gmail.com>
2025-02-12 00:01:30 +0100

    compose fix

--- Dockerfile
+++ Dockerfile
@@ -8,13 +8,12 @@ RUN bun run build
 
 EXPOSE 3000
 ENV HOST=0.0.0.0
-
 ENV PUPPETEER_SKIP_CHROMIUM_DOWNLOAD true
 RUN apt-get update && apt-get install gnupg wget -y && \
-  wget --quiet --output-document=- https://dl-ssl.google.com/linux/linux_signing_key.pub | gpg --dearmor > /etc/apt/trusted.gpg.d/google-archive.gpg && \
-  sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list' && \
-  apt-get update && \
-  apt-get install google-chrome-stable -y --no-install-recommends && \
-  rm -rf /var/lib/apt/lists/*
+    wget --quiet --output-document=- https://dl-ssl.google.com/linux/linux_signing_key.pub | gpg --dearmor > /etc/apt/trusted.gpg.d/google-archive.gpg && \
+    sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list' && \
+    apt-get update && \
+    apt-get install google-chrome-stable -y --no-install-recommends && \
+    rm -rf /var/lib/apt/lists/*
 
 CMD ["bun", "build/index.js"]
--- src/routes/scrape/+server.js
+++ src/routes/scrape/+server.js
@@ -1,31 +1,36 @@
 import puppeteer from 'puppeteer';
 import { json } from '@sveltejs/kit';
 
-
 export async function POST({ request }) {
-    const data = await request.json();
-    //const browser = await puppeteer.launch({headless:true,executablePath: '/usr/bin/google-chrome', args: ['--no-sandbox', '--disable-setuid-sandbox']});
-    const browser = await puppeteer.launch({headless:true, args: ['--no-sandbox', '--disable-setuid-sandbox']});
+	const data = await request.json();
+	const browser = await puppeteer.launch({
+		headless: true,
+		executablePath: '/usr/bin/google-chrome',
+		args: ['--no-sandbox', '--disable-setuid-sandbox']
+	});
+	try {
+		//const browser = await puppeteer.launch({headless:true, args: ['--no-sandbox', '--disable-setuid-sandbox']});
 		const page = await browser.newPage();
-        await page.setContent(
-				data.mkp
-			);
-        await page.addStyleTag({
-            content: data.style
-        })
-        const buffer = await page.pdf({
-				format: 'A4',
-				printBackground: true
-			});
-
-    console.log(data)
-    return new Response(buffer, {
-        status: 200,
-        headers: {
-            'Content-Type': 'application/pdf',
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
-            'Access-Control-Allow-Headers': 'Content-Type'
-        },
-    });
+		await page.setContent(data.mkp);
+		await page.addStyleTag({
+			content: data.style
+		});
+		const buffer = await page.pdf({
+			format: 'A4',
+			printBackground: true
+		});
+		return new Response(buffer, {
+			status: 200,
+			headers: {
+				'Content-Type': 'application/pdf',
+				'Access-Control-Allow-Origin': '*',
+				'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+				'Access-Control-Allow-Headers': 'Content-Type'
+			}
+		});
+	} catch (e) {
+		return new Response(e, { status: 400 });
+	} finally {
+		await browser.close();
+	}
 }
